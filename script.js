89d0a37fe8d1c9cdbb226fe3e0c7253edfead344
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-04-23 16:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let price = document.getElementsByClassName("price")
+
 
 const getSum = () => {
 //Add your code here
-  
+let sum = 0;
+	
+		for(let t of price){
+			// t.innerText
+            // console.log(t)
+            let val = +t.innerText;
+            // console.log(typeof val);
+            // console.log(val);
+            sum += val;
+		}
+	
+//   return sum
+// console.log(sum)
+let div = document.createElement("div");
+div.innerText = sum;
+document.body.appendChild(div);
+
+
 };
 
 getSumBtn.addEventListener("click", getSum);
